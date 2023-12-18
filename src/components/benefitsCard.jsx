a3a4b785c4d07606ca6865ac6db63ec8d5fa17b7
Anushka Verma <anushka@indivue.in>
2023-12-18 11:52:22 +0530

    modified some card sstyling so that text will not overflow from the container

--- src/components/benefitsCard.jsx
+++ src/components/benefitsCard.jsx
@@ -6,7 +6,7 @@ const BenefitsCard = ({src, number, cardHeading, cardPara, bgColor, border}) => 
       <div className='w-[100%] h-[60%] flex justify-center items-center m-auto'>
         {src && <Image src={src} alt='benefits card image' />}
       </div>
-      <div className={` ${bgColor} h-[50%] p-4 border -mt-5 ${border} rounded-xl`}>
+      <div className={` ${bgColor} h-[60%] p-4 border -mt-5 ${border} rounded-xl`}>
         <div className='w-[80%]'>
             <h2 className='text-[#E76F00] text-2xl font-semibold'>{number}</h2>
             <h3 className='font-bold mt-2 text-base'>{cardHeading}</h3>
