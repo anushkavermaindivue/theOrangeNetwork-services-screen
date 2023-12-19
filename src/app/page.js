0bfa5f3bdce799126a737197aa5ead372483f468
Anushka Verma <anushka@indivue.in>
2023-12-19 13:43:02 +0530

    just trying to change something, but didn't change

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Calculator from "@/components/calculator";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <main>
       Home
+      <Calculator />
     </main>
   )
 }
