7f3c3c893cc5695df97a8f2772edd5fa86febd33
Anushka Verma <anushka@indivue.in>
2023-12-19 13:40:52 +0530

    made hero and services component responsive

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,10 +2,12 @@ import Image from 'next/image'
 
 const Hero = ({heroImg, heroTitle}) => {
   return (
-    <section>
-     <Image className='h-[87.55vh] absolute' src={heroImg} alt='background image'  />
-     <div className='flex justify-center items-center h-[87.55vh] font-serif font-semibold w-[60%] tracking-wider text-[#E76F00] text-6xl'>
-        <div className='w-[40%] text-center uppercase'><h1>{heroTitle}</h1></div>
+    <section className=' h-[60vh] sm:h-[75vh] md:h-[100vh]'>
+    {/* <section className='lg:h-[87vh] md:h-[80vh] sm:h-[70vh]'> */}
+     {/* <Image className='h-auto max-w-full absolute' src={heroImg} alt='background image'  /> */}
+     <Image className='lg:h-[87%] md:min-h-[75vh] sm:min-h-[65vh] min-h-[50vh] absolute' src={heroImg} alt='background image'  />
+     <div className='flex justify-center items-center lg:min-h-[87%] md:min-h-[75vh] sm:min-h-[65vh] min-h-[50vh] font-serif font-semibold w-[60%] tracking-wider text-[#E76F00] text-xl sm:text-4xl md:text-5xl lg:text-6xl'>
+        <div className='w-[41%] text-center uppercase'><h1>{heroTitle}</h1></div>
      </div>
     </section>
   )
--- src/components/servicesComp.jsx
+++ src/components/servicesComp.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const ServicesComp = ({servicesTitle, servicesImg, servicesP1, servicesP2}) => {
   return (
-    <section className="max-w-[90%] sm:max-w-[85%] md:max-w-[75%] lg:max-w-[70%] m-auto uppercase mt-28 flex flex-col gap-y-7">
+    <section className="max-w-[90%] sm:max-w-[85%] md:max-w-[75%] lg:max-w-[70%] m-auto uppercase sm:mt-0 md:mt-18 lg:mt-28 flex flex-col gap-y-7">
       <div className="flex flex-col items-center">
         <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-[#141414] flex flex-col items-end">{servicesTitle}<div className="h-0.5 w-28 mt-1 bg-[#E76F00]"></div></h2>
         <Image className="m-auto w-[180px] sm:w-[200px] md:w-[225px]" src={servicesImg} alt="web design image" />
