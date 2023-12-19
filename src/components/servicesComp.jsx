229c7ceaa721766ea94948fa204f4b373ddfec71
Anushka Verma <anushka@indivue.in>
2023-12-19 11:25:28 +0530

    made services component responsive

--- src/components/servicesComp.jsx
+++ src/components/servicesComp.jsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 const ServicesComp = ({servicesTitle, servicesImg, servicesP1, servicesP2}) => {
   return (
-    <section className="max-w-[70%] m-auto uppercase mt-28 flex flex-col gap-y-7">
+    <section className="max-w-[90%] sm:max-w-[85%] md:max-w-[75%] lg:max-w-[70%] m-auto uppercase mt-28 flex flex-col gap-y-7">
       <div className="flex flex-col items-center">
-        <h2 className="text-3xl font-bold text-[#141414] flex flex-col items-end">{servicesTitle}<div className="h-0.5 w-28 mt-1 bg-[#E76F00]"></div></h2>
-        <Image className="m-auto" src={servicesImg} width={225} alt="web design image" />
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-[#141414] flex flex-col items-end">{servicesTitle}<div className="h-0.5 w-28 mt-1 bg-[#E76F00]"></div></h2>
+        <Image className="m-auto w-[180px] sm:w-[200px] md:w-[225px]" src={servicesImg} alt="web design image" />
       </div>
       <div className="font-semibold tracking-wider">
-        <p>
+        <p className="text-[0.6rem] sm:text-[0.8rem] md:text-[1rem]">
             {servicesP1}
         </p>
       </div>
       <div className="font-semibold tracking-wider">
-        <p>
+        <p className="text-[0.6rem] sm:text-[0.8rem] md:text-[1rem]">
             {servicesP2}
         </p>
       </div>
